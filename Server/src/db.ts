f02c59d7d79a0868c74ec5abfad7686c4bd27a91
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-10-11 08:29:11 +1100

    Fix server

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -3,6 +3,9 @@ import { MongoClient, Db } from 'mongodb'
 import env from 'src/env'
 import deduplicate from 'src/common/deduplicate'
 
+// Without these imports, there is no TypeScript error but app will crash because the extension properties are not defined
+import 'src/quiz-app/db/Db+collections'
+
 const client = new MongoClient(env.mongodb.uri)
 let db: Db | undefined
 
