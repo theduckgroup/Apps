b77a17f0dde38b84a0a6e3b8324aaf97fe3a8f16
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-10-19 14:57:45 +1100

    Small

--- WebApp/src/admin-app/pages/EditUserModal.tsx
+++ WebApp/src/admin-app/pages/EditUserModal.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { Modal, Text, TextInput, Button, Group, Grid, Stack } from '@mantine/core'
 import { useForm } from '@mantine/form'
