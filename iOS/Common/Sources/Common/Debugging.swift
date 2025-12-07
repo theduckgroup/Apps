5414a573d0309349f25cc319d6d90acffdcb5512
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-07 14:42:46 +1100

    Debugging impl

--- iOS/Common/Sources/Common/Debugging.swift
+++ iOS/Common/Sources/Common/Debugging.swift
@@ -1,8 +1,22 @@
 import Foundation
 
-public var debugging: Bool {
-    false
-}
+/// Whether the app is running in debugging.
+///
+/// Returns `true` if one of these is true:
+/// - `DEBUG` flag is true
+/// - Running in simulator
+/// - Debugger is attached
+///
+/// Computed once when first accessed and is cached.
+public let debugging: Bool = {
+#if DEBUG || targetEnvironment(simulator)
+    true
+#else
+    // Whether debugger is attached (ie running from Xcode)
+    // This works with Release builds
+    getppid() != 1
+#endif
+}()
 
 public var isRunningForPreviews: Bool {
     ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
