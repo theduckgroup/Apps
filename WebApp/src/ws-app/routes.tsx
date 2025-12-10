f34f6bd22d496efaf826c4ddfaf67e8e6cb91ce9
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-10 12:54:23 +1100

    Fix ws-app/list path

--- WebApp/src/ws-app/routes.tsx
+++ WebApp/src/ws-app/routes.tsx
@@ -4,8 +4,8 @@ import TemplateEditorPage from './pages/TemplateEditorPage'
 
 export default (
   <>
-    <Route index element={<Navigate to='templates' replace />} />
-    <Route path='templates' element={<TemplateListPage />} />
+    <Route index element={<Navigate to='list' replace />} />
+    <Route path='list' element={<TemplateListPage />} />
     <Route path='template' element={<TemplateEditorPage />} />
     <Route path='template/:templateId' element={<TemplateEditorPage />} />
   </>
