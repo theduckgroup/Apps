0ade2117b3c7b72fc4ccb99aff15c76a395af438
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-12 18:26:47 +1100

    WS app - simpler styling code

--- iOS/WeeklySpending/Sources/App View/AppView.swift
+++ iOS/WeeklySpending/Sources/App View/AppView.swift
@@ -10,35 +10,13 @@ struct AppView: View {
     
     var body: some View {
         bodyContent()
-            .onAppear {
+            .tint(appDefaults.accentColor)
+            .preferredColorScheme(appDefaults.colorSchemeOverride?.colorScheme)
+            .onFirstAppear {
                 _ = KeyboardDoneButtonManager.shared
-                applyStylingOverride()
-            }
-            .onChange(of: appDefaults.colorSchemeOverride) {
-                applyStylingOverride()
-            }
-            .onChange(of: appDefaults.accentColor) { oldValue, newValue in
-                applyStylingOverride()
             }
     }
-    
-    private func applyStylingOverride() {
-        let window = UIApplication.shared.anyKeyWindow
-        
-        guard let window else {
-            assertionFailure()
-            return
-        }
-        
-        window.overrideUserInterfaceStyle = switch appDefaults.colorSchemeOverride {
-        case .light: .light
-        case .dark: .dark
-        case .none: .unspecified
-        }
-        
-        window.tintColor = UIColor(appDefaults.accentColor)
-    }
-    
+
     @ViewBuilder
     private func bodyContent() -> some View {
         if auth.isLoaded {
