aa290e7570195b327c1aad6bb434821531942ad9
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-12 17:55:27 +1100

    Add more accent colors & improve layout

--- iOS/Common/Sources/AppUI/AccentColorView.swift
+++ iOS/Common/Sources/AppUI/AccentColorView.swift
@@ -4,40 +4,52 @@ import Flow
 
 struct AccentColorView: View {
     @Binding var accentColor: Color
-    private let colorWithLightness: [(Color, Lightness)] = [
-        (Color.theme, .dark),
-        (Color.red, .dark),
-        (Color.green, .light),
-        (Color.indigo, .dark),
-        (Color.teal, .dark),
-        (Color.yellow, .light),
-        (Color.purple, .dark),
-        (Color.brown, .dark),
+    @State var containerSize: CGSize?
+    
+    private let colorWithLightness: [Color] = [
+        Color.theme,
+        Color.red,
+        Color.orange,
+        Color.yellow,
+        // Color.pink, // Very similar to red
+        Color.purple,
+        Color.blue,
+        Color.indigo,
+        Color.teal,
+        Color.mint,
+        Color.green,
+        Color.brown,
+        Color.gray,
     ]
     
     var body: some View {
+        // Equation:
+        // swatchSize * countPerRow + minSpacing * (countPerRow - 1) <= width
+        // countPerRow <= (width + spacing) / (swatchSize + spacing)
+        // spacing = (width - swatchSize * countPerRow) / (countPerRow - 1)
+        
+        let width = containerSize?.width ?? 0
+        
+        let swatchSize: CGFloat = 32
+        let minSpacing: CGFloat = 15
+        let countPerRow = ((width + minSpacing) / (swatchSize + minSpacing)).rounded(.down)
+        let spacing = (width - swatchSize * countPerRow) / (countPerRow - 1)
+        
         // `distributeItemsEvenly: true`: breaks into two rows with even number of items
-        HFlow(itemSpacing: 15, rowSpacing: 15, justified: true) {
-            ForEach(Array(colorWithLightness.enumerated()), id: \.offset) { index, colorWithLightness in
-                let (color, _) = colorWithLightness
-                
+        // `justified: true`: applies also to the last row!
+        HFlow(itemSpacing: spacing, rowSpacing: minSpacing) {
+            ForEach(Array(colorWithLightness.enumerated()), id: \.offset) { index, color in
                 Button {
                     accentColor = color
                     
                 } label: {
                     Circle()
                         .fill(color)
-                        .frame(width: 32, height: 32)
+                        .frame(width: swatchSize, height: swatchSize)
                         .overlay {
-                            let equal = color.isApproximatelyEqualTo(accentColor)
-                            
-                            if equal {
+                            if color.isApproximatelyEqualTo(accentColor) {
                                 Circle()
-                                    .strokeBorder(
-                                        // lightness == .light ? Color.black.opacity(0.9) : Color.white,
-                                        Color.white,
-                                        lineWidth: 2
-                                    )
+                                    .strokeBorder(Color.white, lineWidth: 2)
                                     .padding(2)
                             }
                         }
@@ -45,11 +57,7 @@ struct AccentColorView: View {
                 .buttonStyle(.plain)
             }
         }
-        .frame(maxWidth: 600) // Must not be .infinite for `justified: true` to work
-    }
-    
-    private enum Lightness {
-        case light
-        case dark
+        .frame(maxWidth: .infinity, alignment: .leading) // `maxWidth: .infinity` is needed for `readSize`
+        .readSize(assignTo: $containerSize)
     }
 }
