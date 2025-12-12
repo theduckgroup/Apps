be9e830619dda9bdfc39e027600c03ff4af3ad10
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-12 17:56:02 +1100

    CurrencyField

--- iOS/Common/Sources/CommonUI/Text Field/CurrencyField.swift
+++ iOS/Common/Sources/CommonUI/Text Field/CurrencyField.swift
@@ -1,92 +1,95 @@
-//import SwiftUI
-//
-//public struct CurrencyField: View {
-//    let name: String
-//    @Binding var value: Double
-//    var onFocus: () -> Void
-//    @State private var text: String
-//    @FocusState private var focused
-//    @ScaledMetric private var fontSize: CGFloat = 20
-//    
-//    public init(
-//        _ name: String,
-//        _ value: Binding<Double>,
-//        onFocus: @escaping () -> Void = { }
-//    ) {
-//        self.name = name
-//        _value = value
-//        self.text = Self.formatValue(value.wrappedValue, unit, restriction)
-//        self.onFocus = onFocus
-//    }
-//    
-//    public var body: some View {
-//        VStack(alignment: .leading, spacing: 0) {
-//            Text(name)
-//                .foregroundStyle(.secondary)
-//                .multilineTextAlignment(.leading)
-//                .font(.body.smallCaps().leading(.tight))
-//                .frame(maxHeight: .infinity, alignment: .bottom)
-//            
-//            TextField("", text: $text)
-//                .focused($focused)
-//                .font(.system(size: fontSize))
-//                .multilineTextAlignment(.leading)
-//                .frame(height: fontSize * 1.75)
-//                .keyboardType(integer ? .numberPad : .decimalPad)
-//        }
-//        .frame(minWidth: 60, alignment: .leading)
-//        // .fixedSize()
-//        .onTapGesture {
-//            focused = true
-//        }
-//        .onChange(of: text) { newValue in
-//            let x = text.components(separatedBy: " ").first ?? "" // Strip unit
-//            value = Double(x) ?? 0
-//        }
-//        .onChange(of: value) { newValue in
-//            if !focused {
-//                text = Self.formatValue(value, unit, restriction)
-//                // print("4 Set text to \(text)")
-//            }
-//        }
-//        .onChange(of: focused) { newValue in
-//            if focused {
-//                if value == 0 {
-//                    text = ""
-//                    
-//                } else {
-//                    text = Self.formatValue(value, restriction)
-//                    // print("1 Set text to \(text)")
-//                }
-//                
-//                onFocus()
-//                
-//            } else {
-//                text = Self.formatValue(value, unit, restriction)
-//                // print("2 Set text to \(text), unit = \(unit ?? "nil")")
-//            }
-//        }
-//    }
-//    
-//    /// Formats value with unit.
-//    private static func formatValue(_ value: Double, _ unit: String?, _ restriction: Restriction) -> String {
-//        var result = formatValue(value, restriction)
-//        
-//        if let unit {
-//            result += " \(unit)"
-//        }
-//        
-//        return result
-//    }
-//    
-//    /// Formats value (without unit).
-//    private static func formatValue(_ value: Double, _ restriction: Restriction) -> String {
-//        value.formatted(
-//            .number
-//                .precision(
-//                    .fractionLength(restriction == .integer ? 0...0 : 0...1)
-//                )
-//                .grouping(.never)
-//        )
-//    }
-//}
+import SwiftUI
+
+public struct CurrencyField: View {
+    var placeholder: String
+    @Binding var value: Decimal
+    var currencySymbol: String = "$"
+    var fractionLength: Int = 2
+    var onFocus: () -> Void
+    @State private var text: String
+    @FocusState private var focused
+    
+    public init(
+        _ placeholder: String,
+        value: Binding<Decimal>,
+        onFocus: @escaping () -> Void = { }
+    ) {
+        self.placeholder = placeholder
+        self._value = value
+        self.text = Self.formatValue(value.wrappedValue, fractionLength: fractionLength, currencySymbol: currencySymbol)
+        self.onFocus = onFocus
+    }
+    
+    public var body: some View {
+        TextField(placeholder, text: $text)
+            .focused($focused)
+            .multilineTextAlignment(.leading)
+            .keyboardType(.decimalPad)
+            .onTapGesture {
+                focused = true
+            }
+            .onChange(of: text) {
+                textChanged()
+            }
+            .onChange(of: value) {
+                valueChanged()
+            }
+            .onChange(of: focused) {
+                focusedChanged()
+            }
+    }
+    
+    private func textChanged() {
+        do {
+            if text == "" {
+                value = 0
+                
+            } else {
+                // This works for both "123456" and "$123,456"
+                value = try Decimal(text, format: .currency(code: "AUD").locale(Locale(identifier: "en_AU")))
+            }
+            
+        } catch {
+            print("Unable to parse '\(text)': \(error)")
+            // assertionFailure()
+        }
+    }
+    
+    private func valueChanged() {
+        if !focused {
+            text = Self.formatValue(value, fractionLength: fractionLength, currencySymbol: currencySymbol)
+        }
+    }
+    
+    private func focusedChanged() {
+        if focused {
+            if value == 0 {
+                text = ""
+                
+            } else {
+                text = Self.formatValue(value, fractionLength: fractionLength)
+                // print("1 Set text to \(text)")
+            }
+            
+            onFocus()
+            
+        } else {
+            text = Self.formatValue(value, fractionLength: fractionLength, currencySymbol: currencySymbol)
+            // print("2 Set text to \(text), unit = \(unit ?? "nil")")
+        }
+    }
+    
+    /// Formats value with currency symbol.
+    private static func formatValue(_ value: Decimal, fractionLength: Int, currencySymbol: String) -> String {
+        currencySymbol + formatValue(value, fractionLength: fractionLength)
+    }
+    
+    /// Formats value without currency symbol.
+    private static func formatValue(_ value: Decimal, fractionLength: Int) -> String {
+        value.formatted(
+            .number
+                .precision(.fractionLength(0...fractionLength))
+                .grouping(.automatic)
+        )
+    }
+}
