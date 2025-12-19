1d201abf8e7425ce447e98f1e1f2bd6a774b27e8
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-20 10:11:11 +1100

    Rework color based on OKLCH and original color

--- iOS/Common/Sources/CommonUI/Extensions/UIColor+Theme.swift
+++ iOS/Common/Sources/CommonUI/Extensions/UIColor+Theme.swift
@@ -2,11 +2,18 @@ import Foundation
 import UIKit
 
 public extension UIColor {
-    /// Lighter theme color (used for dark UI style).
-    static let themeLight = UIColor(hex: 0x2E7DB1)
-    // static let themeLight = UIColor(hex: 0x2F80AD) // Too light
+    // Original: #042434 | oklch(0.2464 0.0474 234.89)
+    // Theme colors are calculated by changing original's chroma to 0.1, then varying lightness
+    // Use OKCLH picker: https://oklch.com/
+    // Make sure you use okclh format rather than rgb to avoid conversion errors
     
-    /// Darker theme color (used for light UI style).
-    static let themeDark = UIColor(hex: 0x2C6494)
-    // static let themeDark = UIColor(hex: 0x286090) // Too dark
+    /// Lighter theme color (used for dark UI style, e.g. text on top of dark background).
+    static let themeLight = UIColor(hex: 0x3989b3) // oklch(0.6 0.1 234.89)
+    
+    /// Darker theme color (used for light UI style, e.g. text on top of light background).
+    static let themeDark = UIColor(hex: 0x126b94) // oklch(0.5 0.1 234.89)
+    
+    // Old colors
+    // static let themeLight = UIColor(hex: 0x2E7DB1)
+    // static let themeDark = UIColor(hex: 0x2C6494)
 }
