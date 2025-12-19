3d7ebc63f267075644f9c70fe7dd4ea9ddcb1ad4
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-19 23:06:18 +1100

    Adjust theme color

--- iOS/Common/Sources/CommonUI/Extensions/UIColor+Theme.swift
+++ iOS/Common/Sources/CommonUI/Extensions/UIColor+Theme.swift
@@ -2,8 +2,11 @@ import Foundation
 import UIKit
 
 public extension UIColor {
-    static let themeLight = UIColor(hex: 0x2F80AD)
-    // static let themeLight = UIColor(hex: 0x2E7DB1)
+    /// Lighter theme color (used for dark UI style).
+    static let themeLight = UIColor(hex: 0x2E7DB1)
+    // static let themeLight = UIColor(hex: 0x2F80AD) // Too light
+    
+    /// Darker theme color (used for light UI style).
     static let themeDark = UIColor(hex: 0x2C6494)
-    // static let themeDark = UIColor(hex: 0x286090)
+    // static let themeDark = UIColor(hex: 0x286090) // Too dark
 }
