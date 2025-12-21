5005c1da12003c0399cbf41c8be1e2dc222aee86
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-21 14:41:07 +1100

    Move AppView into separate file

--- iOS/SuperDuck/App/App.swift
+++ iOS/SuperDuck/App/App.swift
@@ -1,13 +1,8 @@
 import SwiftUI
-import AppShared
 import Backend
-import CommonUI
-import QuizApp
 
 @main
 struct App: SwiftUI.App {
-    @State var appDefaults = AppDefaults(storageKey: "appDefaultsV2")
-    
     var body: some Scene {
         WindowGroup {
             AppView()
@@ -16,68 +11,4 @@ struct App: SwiftUI.App {
                 }
         }
     }
-}
-
-private struct AppView: View {
-    var auth = Auth.shared
-    @Environment(AppDefaults.self) var appDefaults
-    @State var uikitContext = UIKitContext()
-    
-    init() {}
-    
-    var body: some View {
-        // Note:
-        // - Changing preferredColorScheme does not change already presented views
-        // - window.overrideUserInteraceStyle doesn't work perfectly either (e.g. in Home View -> Settings popover on iPad) but better than preferredColorScheme
-        // - Can't mix preferredColorScheme and window.overrideUserInteraceStyle
-        // - `tint` works fine
-        
-        bodyContent()
-            .tint(appDefaults.accentColor)
-            .onFirstAppear {
-                _ = KeyboardDoneButtonManager.shared
-                applyStylingOverride()
-            }
-             .onChange(of: appDefaults.colorSchemeOverride, applyStylingOverride)
-            .attach(uikitContext)
-            .environment(appDefaults)
-    }
-    
-    private func applyStylingOverride() {
-        uikitContext.onAddedToWindow {
-            guard let window = uikitContext.viewController.view.window else {
-                assertionFailure()
-                return
-            }
-            
-            window.overrideUserInterfaceStyle = switch appDefaults.colorSchemeOverride {
-            case .light: .light
-            case .dark: .dark
-            case .none: .unspecified
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private func bodyContent() -> some View {
-        if auth.isLoaded {
-            if auth.user != nil {
-                TabView()
-                
-            } else {
-                LoginView()
-            }
-        } else {
-            ProgressView()
-                .progressViewStyle(.circular)
-                .scaleEffect(1.5)
-                .tint(.secondary)
-        }
-    }
-}
-
-#Preview {
-    AppView()
-        .tint(.theme)
-        .environment(AppDefaults.mock)
 }
