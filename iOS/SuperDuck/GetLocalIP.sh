9b8b1be14a2699712337e250f2502bb9411461a1
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-21 17:42:14 +1100

    Remove Local IP Run Script

--- iOS/SuperDuck/GetLocalIP.sh
+++ iOS/SuperDuck/GetLocalIP.sh
@@ -4,5 +4,5 @@
 # Make sure you open LocalIP in VS Code to see this effect because Xcode automatically inserts new line when you open a file
 # Have tried echo -n and xargs, did not work
 
-echo "Obtaining local IP address"
+echo "Get local IP and write to LocalIP"
 ipconfig getifaddr en0 | tr -d '\n' > ./LocalIP
