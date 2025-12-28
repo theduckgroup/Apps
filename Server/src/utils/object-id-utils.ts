3e5e1f467af12009aabd0d7a20acb4d9720d7906
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-28 12:05:51 +1100

    object-id-utils

--- Server/src/utils/object-id-utils.ts
+++ Server/src/utils/object-id-utils.ts
@@ -49,3 +49,14 @@ export function objectIdPropertyToString<T, K extends KeysOfValue<T, ObjectId>>(
 
   return transform(data)
 }
+
+
+
+/**
+ * Transforms `_id: ObjectId` to `id: string`.
+ */
+export function jsonifyMongoId<T extends { _id: ObjectId }>(object: T) {
+  const { _id, ...rest } = object
+  const obj = { ...rest, id: _id.toString() }
+  return obj
+}
