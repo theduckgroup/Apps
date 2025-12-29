7fbaf0e71c22796a20bfc140ae230f1e22c6a151
Khanh Nguyen <theduckgroupapp@gmail.com>
2025-12-29 15:09:31 +1100

    Mantine teal

--- iOS/Common/Sources/CommonUI/ColorExtensions/Color+Theme.swift
+++ iOS/Common/Sources/CommonUI/ColorExtensions/Color+Theme.swift
@@ -2,15 +2,19 @@ import Foundation
 import SwiftUI
 
 public extension Color {
-    static let theme = Color(
-        UIColor { traitCollection in
-            traitCollection.userInterfaceStyle == .dark ? UIColor.themeLight : UIColor.themeDark
-        }
-    )
+    // Theme
     
+    static let theme = Color(UIColor.theme)
     static let themeLight = Color(UIColor.themeLight)
     static let themeDark = Color(UIColor.themeDark)
     
-    /// Very dark color used in NakedBlendCalc for nav bar, not useful for text etc.
+    // Very dark color used in NakedBlendCalc for nav bar, not useful for text etc.
+    
     static let themeAlt2 = Color(hex: 0x153850)
+    
+    // Mantine Teal
+    
+    static let mantineTeal = Color(UIColor.mantineTeal)
+    static let mantineTealLight = Color(UIColor.mantineTealLight)
+    static let mantineTealDark = Color(UIColor.mantineTealDark)
 }
