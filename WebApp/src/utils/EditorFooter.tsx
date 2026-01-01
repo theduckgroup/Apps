1e37d439b9c331e5c92c3cefb17dbf0631487bb9
Khanh Nguyen <theduckgroupapp@gmail.com>
2026-01-01 16:42:42 +1100

    Refactor EditorFooter

--- WebApp/src/utils/EditorFooter.tsx
+++ WebApp/src/utils/EditorFooter.tsx
@@ -12,21 +12,24 @@ export function EditorFooter({ editorRef, hasUnsavedChanges, save, isSaving, sav
 }) {
   const viewportSize = useViewportSize()
   const editorRect = useElementRect(editorRef)
+  const height = 80
 
   return (
     <>
-      <div className='h-26' />
+      <div className='mt-6' style={{ height: height }} />
       <div
         className='
-          fixed left-0 right-0 bottom-0 h-22 pb-[env(safe-area-inset-bottom)]
+          fixed left-0 right-0 bottom-0 pb-[env(safe-area-inset-bottom)]
           bg-[var(--mantine-color-dark-7)]
+          border-t border-neutral-700
         '
         style={{
+          height: height,
           paddingLeft: editorRect?.left ?? 0,
           paddingRight: viewportSize.width - (editorRect?.right ?? 0)
         }}
       >
-        <div className='w-full h-full flex flex-row justify-between items-center px-4 py-4 gap-4'>
+        <div className='w-full h-full flex flex-row justify-between items-center px-4 pt-3 pb-6 gap-4'>
           {saveError ? <Text c='red' lineClamp={2} lh='xs'>{formatError(saveError)}</Text> : <div />}
           <Button
             onClick={save}
