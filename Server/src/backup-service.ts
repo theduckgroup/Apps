2b312ed40d91045e81e75d1168d53f62fd7b5767
Khanh Nguyen <theduckgroupapp@gmail.com>
2026-01-03 21:43:45 +1100

    Change retention period for dev test

--- Server/src/backup-service.ts
+++ Server/src/backup-service.ts
@@ -414,7 +414,7 @@ const config: Config = (() => {
     case 'development':
       return {
         checkIntervalMs: 30 * 1000,
-        retentionMs: 12 * 60 * 1000,
+        retentionMs: 3 * 60 * 1000,
         isSamePeriodFn: isSameMinuteUTC,
         supabaseBucket: 'apps-dev',
         supabaseBackupFolder: 'db-backup',
